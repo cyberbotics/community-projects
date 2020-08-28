Add full rotation and fix bugs

default/controllers/generic_inverse_kinematic/generic_inverse_kinematic.py
@@ -28,6 +28,9 @@ from spawn_target import spawnTarget
 # is only relevant, if the target is constantly changing, as no new IK
 # solution gets calculated, if the target did not change.
 IKstepSize = 10 
+useRotation = True
+last_link_vector = None#[0, 0.12, 0]
+
 
 # ----------------------------------------------------------
 # ----------------------------------------------------------
@@ -39,7 +42,7 @@ if not supervisor.getSupervisor():
 
 timeStep = int(supervisor.getBasicTimeStep())
 # Initialize our inverse kinematics module
-ik = inverseKinematics(supervisor)
+ik = inverseKinematics(supervisor,last_link_vector)
 
 # check if our world already has the TARGET node. If not, we spawn it.
 target = supervisor.getFromDef('TARGET')
@@ -62,8 +65,13 @@ for i in range(n):
     #print(device.getName(), '   - NodeType:', device.getNodeType())
     if device.getNodeType() == 54:
         motors.append(device)
-        sensors.append(device.getPositionSensor())
-        sensors[-1].enable(timeStep)       
+        sensor = device.getPositionSensor()
+        try:
+            sensor.getName()
+            sensors.append(sensor)
+            sensor.enable(timeStep)  
+        except:
+            print('Rotational Motor: ' + device.getName() + ' has no Position Sensor')
        
  
 target_pos_old = np.zeros((3))
@@ -76,7 +84,10 @@ while supervisor.step(IKstepSize*timeStep) != -1:
     # check if our TARGET position or rotation has changed. If not, skip ik calculations (computationally intensive)
     if not np.array_equal(target_pos, target_pos_old) or not np.array_equal(target_rot, target_rot_old):
         # Call the ik_module to compute the inverse kinematics of the arm.   
-        ikResults = ik.get_ik(target_pos, target_rot)
+        if useRotation:
+            ikResults = ik.get_ik(target_pos, target_rot)
+        else: 
+            ikResults = ik.get_ik(target_pos)
         # set the motor positions to the calculated ik solution
         for i in range(len(motors)):
             motors[i].setPosition(ikResults[i + 1])

default/controllers/generic_inverse_kinematic/ik_module.py
@@ -32,7 +32,7 @@ if ikpy.__version__[0] < '3':
              'Please upgrade "ikpy" Python module to version "3.0" or newer with this command: "pip install --upgrade ikpy"')
 
 class inverseKinematics():
-    def __init__(self, supervisor):
+    def __init__(self, supervisor, last_link_vector=None):
         # Initialize the Webots Supervisor.
         self.supervisor = supervisor
         self.timeStep = int(self.supervisor.getBasicTimeStep())
@@ -43,41 +43,62 @@ class inverseKinematics():
         self.sensor_names = []
         for i in range(n):
             device = self.supervisor.getDeviceByIndex(i)
+            print(device.getName() , '  -  NodeType = ' , device.getNodeType())
             if device.getNodeType() == 54:
                 sensor = device.getPositionSensor()
                 self.motor_names.append(device.getName())
-                self.sensor_names.append(sensor.getName())
+                try:
+                    self.sensor_names.append(sensor.getName())
+                except:
+                    print('Rotational Motor: ' + device.getName() + ' has no Position Sensor')
 
      
-        # Create the arm chain.
+        # Create the arm chain.         
         filename = None
+        # (uncomment next two lines, if you want to save a copy of the generated urdf file)
+        with open('filename.urdf',  'w') as file:
+            file.write(supervisor.getUrdf())   
         with tempfile.NamedTemporaryFile(suffix='.urdf', delete=False) as file:
             filename = file.name
-            file.write(self.supervisor.getUrdf().encode('utf-8'))
-        self.armChain = ikpy.chain.Chain.from_urdf_file(filename)
+            file.write(self.supervisor.getUrdf().encode('utf-8'))        
+        self.armChain = ikpy.chain.Chain.from_urdf_file(filename) #,last_link_vector=last_link_vector)
+        
 
         # make sure only links with a motor are active for IK calculations
         active_links = [False] * len(self.armChain.links)
-        for i in range(len(self.armChain.links)):  
+        for i in range(len(self.armChain.links)):
             link_name = self.armChain.links[i].name
             active_links[i] = link_name in self.motor_names or link_name in self.sensor_names
             if active_links[i]:
                 # ikpy includes the bounds as valid, In Webots they have to be less than the limit
-                new_lower = self.armChain.links[i].bounds[0] + 0.0000001
-                new_upper = self.armChain.links[i].bounds[1] - 0.0000001       
-                self.armChain.links[i].bounds = (new_lower, new_upper)            
+                new_lower = new_upper = None
+                if self.armChain.links[i].bounds[0] is not None:
+                    new_lower = self.armChain.links[i].bounds[0] + 0.0000001
+                if self.armChain.links[i].bounds[1] is not None:
+                    new_upper = self.armChain.links[i].bounds[1] - 0.0000001       
+                self.armChain.links[i].bounds = (new_lower, new_upper)   
+                self.last_active = i  
+        if not any(active_links):
+            print('WARNING: could not identify which links are active. Setting all links to active by default.')      
+            active_links = [True] * len(self.armChain.links)    
         self.armChain.active_links_mask = active_links    
+        print(self.armChain)
 
         # ----------------------------------------------------------
         # Setup for correct orientation calculations
         # ----------------------------------------------------------
 
         # convert urdf rpy of last link into rotation matrix
-        r = R.from_euler('xyz', self.armChain.links[-1].orientation, degrees=False).as_matrix()
+        try:
+            self.last_link_rot = R.from_euler('xyz', self.armChain.links[-1].orientation, degrees=False).as_matrix()
+        except:
+            print('Last link has no orientation. Parsing identity matrix (no rotation).')
+            self.last_link_rot = np.identity(3)
         # get the translation vector of the last link
         pos = self.armChain.links[-1].translation_vector
         # calculate the final link vector using the dot product
-        self.final_link_vector = np.round(np.dot(r,pos), 2)
+        self.final_link_vector = np.round(np.dot(self.last_link_rot,pos), 2)
+        print('---final vector: ',self.final_link_vector)
 
         # which column we have to take from the rotation matrix, for our toolSlot axis
         try:
@@ -87,13 +108,37 @@ class inverseKinematics():
             print('WARNING!!!!!!!!!!!')
             print('The vector of the last link is not an axis. Make sure the orientation and translation of this link are set up in a way, that either the x, y, or z axis points out of the toolSlot')
             print('The IK solver will not solve correctly for orientation. The controller will now use the z-axis as the final link vector')
-            print('rotationi matrix:')
-            print(r)
             print('translation vector: ', pos)
             print('final link vector:', self.final_link_vector)   
 
         # define for ikpy, which axis we are using for the last link
         self.toolSlot_axis = ['X', 'Y', 'Z'][self.rot_index]
+        self.direction = None
+
+
+    def rotateToolSlot(self, target_rot, ikResults):
+        # calculate the difference in Rotation around the axis between what we requested, and what we computed
+        fk = self.armChain.forward_kinematics(ikResults)
+        # rotation of the calculated ik solution
+        R_fk = fk[:3,:3]
+        # rotation to get from our ik rotation to our target rotation
+        R_new = np.dot(target_rot, R_fk)
+        # convert our rotation matrix into euler angles, so we know how much we have to change the last motor
+        r = R.from_matrix(R_new)
+        euler = r.as_euler('xyz', False)
+        # the first time we call this function we have to figure out in which direction we have to rotate
+        if self.direction is not None: 
+            ikResults[6] = ikResults[self.last_active ] + self.direction * euler[self.rot_index]
+        else:
+            # here we test adding the delta-angle, if the resulting rotation - target rotation is not 0, we flip the direction
+            ikResults[6] = ikResults[self.last_active ] + 1 * euler[self.rot_index]
+            fk_check = self.armChain.forward_kinematics(ikResults)
+            if np.sum(np.abs(np.round(fk_check[:3, :3] - target_rot, 3))) == 0:
+                self.direction = 1
+            else:
+                self.direction = -1
+        return ikResults
+
 
     def get_ik(self, target_pos, target_rot=None):   
         # check if a target_rot is requested (3x3 numpy array)
@@ -105,11 +150,22 @@ class inverseKinematics():
             rot_vector = [target_rot[0,self.rot_index],target_rot[1,self.rot_index],target_rot[2,self.rot_index]]
             
             # Call "ikpy" to compute the inverse kinematics of the arm WITH orientation   
+            transMat = np.identity(4)
+            transMat[:3,:3] = target_rot
+            transMat[:3,3] = target_pos
             ikResults = self.armChain.inverse_kinematics(target_pos, target_orientation=rot_vector, orientation_mode=self.toolSlot_axis)
-
             # if ikResults is all zeros, we are dealing with a singularity
-            if sum(ikResults) == 0: 
+            if sum(abs(ikResults)) < 0.001:  
                 # tiny change in the orientation vector to avoid singularity
-                rot_vector_new = np.array(rot_vector) + np.full((1,3), 0.0000001) 
-                ikResults = self.armChain.inverse_kinematics(target_pos, target_orientation=rot_vector_new, orientation_mode=toolSlot_axis)
-            return ikResults
+                rot_vector_new = np.array(rot_vector) + np.full((1,3), 0.0000001)                 
+                ikResults = self.armChain.inverse_kinematics(target_pos, target_orientation=rot_vector_new, orientation_mode=self.toolSlot_axis)
+                # if the above step was not enough to avoid the singularity, we try to increase the deviation from the requested orientation step by step
+                n = -5
+                while sum(abs(ikResults)) < 0.001: 
+                    print('Simple singulaity avoidance was not suffiecient. Trying larger deviation from requested orientation.')
+                    print('Requested rotation vector: ' + str(rot_vector))
+                    rot_vector_new = np.array(rot_vector) + np.full((1,3), 1 * 10 ** n) 
+                    print('New rotation vector: ' + str(rot_vector_new))
+                    ikResults = self.armChain.inverse_kinematics(target_pos, target_orientation=rot_vector_new, orientation_mode=self.toolSlot_axis)
+                    n += 1            
+            return self.rotateToolSlot(target_rot, ikResults)